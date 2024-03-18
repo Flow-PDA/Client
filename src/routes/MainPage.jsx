43b13b5aae4a3f8004a2eab88f786a4c29001453
sunyoung <4446207@naver.com>
2024-03-18 10:51:01 +0900

    Setting up project structure

--- src/routes/MainPage.jsx
+++ src/routes/MainPage.jsx
@@ -1,7 +1,12 @@
+import logo from "../assets/logo.svg";
+
 const MainPage = () => {
   return (
     <div>
-      <div>메인페이지</div>
+      <img src={logo} alt="Logo" />
+      <h2>함께하는 투자</h2>
+      <div>FLOW와 함께라면 쉬워요</div>
+      <div>지금 모임을 만들고 투자를 시작해보세요!</div>
     </div>
   );
 };
