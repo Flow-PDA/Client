3b4e12785bc0ce1458cf632e0953d1bf84a26c7b
sunyoung <4446207@naver.com>
2024-03-19 15:18:24 +0900

    refactor: InterestPage 수정 및 컴포넌트화

--- src/routes/Interest/InterestPage.jsx
+++ src/routes/Interest/InterestPage.jsx
@@ -2,34 +2,33 @@ import { Button, Col, Container, Row } from "react-bootstrap";
 import TopNavigationBar from "../../components/common/nav/TopNavigationBar";
 import InterestButton from "../../components/common/button/InterestButton";
 import "./InterestPage.css";
-const InterestPage = () => {
-  return (
-    <Container>
-      <TopNavigationBar text={"종목 찜 목록"} />
-      <InterestButton text1="승인중" text2="승인완료" />
-      <Row>
-        <Col xs={2}>3.6</Col>
-        <Col xs={6}>
-          <Row>삼성전자</Row>
-          <Row>최투자</Row>
-        </Col>
-        <Col xs={4}>
-          <Button>종목뺴기</Button>
-        </Col>
-        <hr />
-      </Row>
+import HorizontalLine from "../../components/line/HorizontalLine";
+import InterestPending from "./InterestPending";
+import { useState } from "react";
+import InterestApproval from "./InterestApproval";
 
-      <Row>
-        <Col xs={2}>3.6</Col>
-        <Col xs={6}>
-          <Row>삼성전자</Row>
-          <Row>최투자</Row>
-        </Col>
-        <Col xs={4}>
-          <Button>종목뺴기</Button>
-        </Col>
-      </Row>
-    </Container>
+const InterestPage = () => {
+  const [activeButton, setActiveButton] = useState(0); // 초기값은 0으로 설정
+
+  // 버튼 클릭 이벤트 핸들러
+  const handleButtonClick = (buttonIndex) => {
+    setActiveButton(buttonIndex);
+  };
+
+  return (
+    <>
+      <TopNavigationBar text={"종목 찜 목록"} />
+      <Container>
+        <InterestButton
+          className="interest-state-button"
+          text1="승인중"
+          text2="승인완료"
+          activeButton={activeButton} // 현재 활성화된 버튼 상태를 전달
+          onClick={handleButtonClick} // 버튼 클릭 이벤트 핸들러를 전달
+        />
+        {activeButton === 0 ? <InterestPending /> : <InterestApproval />}
+      </Container>
+    </>
   );
 };
 
