9a5e4c556c07c939f1f2165b9728168bac5168ec
chanjin1998 <chanjin@kookmin.ac.kr>
2024-03-21 13:38:41 +0900

    feat: 광고 swiper 추가

---
+++ src/components/common/swiper/Swiper.jsx
@@ -0,0 +1,52 @@
+import { Swiper, SwiperSlide } from "swiper/react"; // basic
+import SwiperCore from "swiper";
+import { Navigation, Pagination, Autoplay } from "swiper/modules";
+import Strategy from "../../../assets/banner_strategy.jpeg";
+import investBanner from "../../../assets/invest_banner.png";
+import hotBanner from "../../../assets/hot_banner.png";
+import marketBanner from "../../../assets/market_banner.png";
+
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "./Swiper.css";
+SwiperCore.use([Navigation, Pagination, Autoplay]); // Swiper
+
+export default function Swipe(props) {
+  return (
+    <Swiper
+      style={{
+        height: "20vh",
+        width: "100vw",
+      }}
+      spaceBetween={20}
+      slidesPerView={1}
+      navigation
+      pagination={{ clickable: true }}
+      autoplay={{ delay: 2000, disableOnInteraction: false }}
+      loop={true}
+    >
+      <SwiperSlide>
+        <img
+          src={investBanner}
+          alt="banner"
+          style={{ width: "100%", height: "75%" }}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src={hotBanner}
+          alt="banner"
+          style={{ width: "100%", height: "75%" }}
+        />
+      </SwiperSlide>
+      <SwiperSlide>
+        <img
+          src={marketBanner}
+          alt="123"
+          style={{ width: "100%", height: "75%" }}
+        />
+      </SwiperSlide>
+    </Swiper>
+  );
+}
