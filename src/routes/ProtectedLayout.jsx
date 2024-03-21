5473dc0935d783cae03bf6457da3da9723b02004
bkkmw <roboticts49@gmail.com>
2024-03-21 15:27:43 +0900

    feat: add protected layout - redirect to main page

---
+++ src/routes/ProtectedLayout.jsx
@@ -0,0 +1,29 @@
+import { Navigate, Outlet, useLocation } from "react-router-dom";
+import { useSelector } from "react-redux";
+import { useCallback } from "react";
+
+export const ProtectedLayout = () => {
+  const auth = useSelector((state) => state.user.userInfo?.accessToken);
+  const location = useLocation();
+
+  const authRequired = useCallback(() => {
+    const allowList = ["/", "/signup", "/login"];
+    const res = location.pathname in allowList;
+    return !allowList.includes(location.pathname);
+  }, [location]);
+
+  const checkAuth = useCallback((auth) => {
+    if (!auth) return false;
+    return auth !== "";
+  }, []);
+
+  if (authRequired() && !checkAuth(auth)) {
+    return <Navigate to="/" />;
+  } else {
+    return (
+      <>
+        <Outlet />
+      </>
+    );
+  }
+};
