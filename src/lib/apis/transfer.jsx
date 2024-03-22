1b9685773c6dcdaf9eb46118dbeecc6d5474c6eb
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-03-22 09:55:07 +0900

    fix: authInstance로 변경

--- src/lib/apis/transfer.jsx
+++ src/lib/apis/transfer.jsx
@@ -3,14 +3,14 @@ import { authInstance } from "./base";
 
 // 이체 내역
 export async function fetchTransferList(partyKey) {
-  const response = await instance.get(`/transfers/${partyKey}`);
+  const response = await authInstance.get(`/transfers/${partyKey}`);
 
   return response.data.result;
 }
 
 //최근 보낸 계좌
 export async function fetchRecentAccountList(partyKey) {
-  const response = await instance.get(`/transfers/${partyKey}/recents`);
+  const response = await authInstance.get(`/transfers/${partyKey}/recents`);
   return response.data.result;
 }
 
