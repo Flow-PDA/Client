b509c4570338464db8eca47f32d0394e57cff225
chanjin1998 <chanjin@kookmin.ac.kr>
2024-03-25 08:03:54 +0900

    fix: import 경로 변경

--- src/routes/interest/intereststock/chart/InterestStockDetailChartPage.jsx
+++ src/routes/interest/intereststock/chart/InterestStockDetailChartPage.jsx
@@ -1,8 +1,8 @@
 import { Button, Col, Container, Row } from "react-bootstrap";
-import TopNavigationBar from "../../../../components/common/nav/TopNavigationBar";
+import TopNavigationBar from "../../../../components/common/nav/TopNavigationBar.jsx";
 import "./InterestStockDetailChartPage.css";
-import SampleChart from "./SampleChart";
-import StockDataFetcher from "./StockDataFetcher.js";
+import SampleChart from "./SampleChart.jsx";
+import StockDataFetcher from "./StockDataFetcher.js.jsx";
 
 export default function InterestStockDetailChartPage() {
   return (
