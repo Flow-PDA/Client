64a176b64f8e0f40de8321cd8bc4bf545de86e93
bkkmw <roboticts49@gmail.com>
2024-03-29 13:35:13 +0900

    feat: add ws connection at LiveStockPage

--- src/routes/invest/livestock/LiveStockPage.jsx
+++ src/routes/invest/livestock/LiveStockPage.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect, Suspense } from "react";
+import React, { useState, useEffect, Suspense, useCallback } from "react";
 import "./LiveStockPage.css";
 import axios from "axios";
+import io from "socket.io-client";
 import Swipe from "../../../components/common/swiper/Swiper";
 import TopNavigationBar from "../../../components/common/nav/TopNavigationBar";
 import { Row, Col, Container } from "react-bootstrap";
@@ -9,22 +10,23 @@ import { Link, useParams, useNavigate } from "react-router-dom";
 import Search from "../../../assets/search.png";
 import logo from "../../../assets/logo_white.png";
 
+const WS_URL = import.meta.env.VITE_WS_URL;
+
 export default function LiveStockPage() {
   const [selectedIndex, setSelectedIndex] = useState(1);
   const [kospiDatas, setKospiDatas] = useState([]);
   const [kosdaqDatas, setKosdaqDatas] = useState([]);
   const [nasdaqDatas, setNasdaqDatas] = useState([]);
   const [issueDatas, setIssueDatas] = useState([]);
+  const [stockCodeList, setStockCodeList] = useState([]);
   const [stockDatas, setStockDatas] = useState([]);
+  const [socketIo, setSocketIo] = useState(null);
+  const [updatedData, setUpdatedData] = useState(null);
   const partyKey = useParams().partyKey;
   const navigate = useNavigate();
   const handleClick = (tag) => {
     console.log(tag);
     setSelectedIndex(tag);
-    fetchIssueData(tag);
-  };
-
-  const priceData = (tag) => {
     fetchIssueData(tag);
   };
 
@@ -55,7 +57,7 @@ export default function LiveStockPage() {
       const response = await axios.get(
         "http://localhost:3000/api/points/nasdaq"
       );
-      console.log(response.data[0]);
+      // console.log(response.data[0]);
       setNasdaqDatas(response.data[0]);
     } catch (error) {
       console.error("Error fetching data:", error);
@@ -90,7 +92,7 @@ export default function LiveStockPage() {
                     prdy_ctrt: stock_res.data.prdy_ctrt,
                     stck_prpr: stock_res.data.stck_prpr,
                   };
-                  console.log(tmp_data);
+                  // console.log(tmp_data);
                   // const items = [...stockDatas];
                   items.push(tmp_data);
                   console.log(items);
@@ -98,8 +100,8 @@ export default function LiveStockPage() {
                 });
             })
           ).then((items) => {
-            console.log(items);
             setStockDatas(items);
+            setStockCodeList(items.map((elem) => elem[0]["stock_code"]));
           });
         });
     } catch (err) {
@@ -112,7 +114,80 @@ export default function LiveStockPage() {
     fetchKosdaqData();
     fetchNasdaqData();
     fetchIssueData(1);
+
+    // init WS connection
+    if (WS_URL !== undefined) {
+      const _socketIo = io.connect(WS_URL);
+      _socketIo.on("connect", () => {
+        console.log("socket connected");
+      });
+      _socketIo.on("update", (data) => {
+        // console.log(data);
+        setUpdatedData(data);
+      });
+
+      setSocketIo(_socketIo);
+    } else {
+      console.log("WS URL not defined");
+    }
   }, []); // 빈 배열을 넘겨주어 한 번만 실행되도록 설정
+
+  // received new data
+  useEffect(() => {
+    if (stockDatas.length > 0 && updatedData != null) {
+      // console.log(updatedData[0]);
+      const newData = stockDatas.map((elem) => {
+        if (elem[0]["stock_code"] == updatedData[0]) {
+          // if (elem[0]["stck_prpr"] != updatedData[1]) {
+          //   console.log(
+          //     `MODIFIED : ${elem[0]["stck_prpr"]} - ${updatedData[1]}`
+          //   );
+          // }
+          return [
+            {
+              ...elem[0],
+              stck_prpr: updatedData[1],
+              prdy_vrss_sign: updatedData[2],
+              prdy_vrss: updatedData[3],
+              prdy_ctrt: updatedData[4],
+            },
+          ];
+        } else return [elem[0]];
+      });
+      setUpdatedData(null);
+      // console.log(newData);
+      setStockDatas(newData);
+    }
+  }, [updatedData, stockDatas]);
+
+  // list modified
+  useEffect(() => {
+    console.log(stockCodeList);
+    console.log(`REGISTER ${stockCodeList}`);
+
+    stockCodeList.forEach((elem) => {
+      const temp = `1|${elem}`;
+      socketIo.emit("REGISTER_SUB", temp);
+    });
+    return () => {
+      console.log(`RELEASE ${stockCodeList}`);
+      stockCodeList.forEach((elem) => {
+        const temp = `1|${elem}`;
+        socketIo.emit("RELEASE_SUB", temp);
+      });
+    };
+  }, [stockCodeList]);
+
+  // Close socketio
+  useEffect(() => {
+    return () => {
+      console.log("disconnect");
+      if (socketIo != null) {
+        socketIo.disconnect();
+      }
+    };
+  }, [socketIo]);
+
   return (
     <div style={{ position: "relative" }}>
       <TopNavigationBar text={"주식 정보"}></TopNavigationBar>
