18f657276e58cf92e9e4b5b953a08c63c05fb305
ekgus9701@gmail.com <52192706+ekgus9701@users.noreply.github.com>
2024-03-31 00:19:45 +0900

    feat: 주식 실시간 체결가 추가

--- src/routes/interest/intereststock/TradeStockPage.jsx
+++ src/routes/interest/intereststock/TradeStockPage.jsx
@@ -6,14 +6,15 @@ import { useEffect, useState } from "react";
 import PrimaryButton from "../../../components/common/button/PrimaryButton";
 import { useLocation, useParams } from "react-router-dom";
 import { tradeStock } from "../../../lib/apis/hankookApi";
+import io from "socket.io-client";
+import { fetchPartyInfo } from "../../../lib/apis/party";
 
 export default function TradeStockPage() {
-
   const { partyKey, stockKey } = useParams();
   const [price, setPrice] = useState("");
   const [amount, setAmount] = useState("");
   const [isMarketPrice, setIsMarketPrice] = useState(false);
-
+  const [partyInfo, setPartyInfo] = useState();
 
   const location = useLocation();
   const stockName = location.state.stockName;
@@ -22,7 +23,42 @@ export default function TradeStockPage() {
 
   const maxBuyQuantity = async () => {};
 
-  useEffect(() => {}, []);
+  // state for socketIo
+  const [socketIo, setSocketIo] = useState(null);
+
+  let [stockExecutionPrice, setStockExecutionPrice] = useState(0);
+  // when mounted
+  useEffect(() => {
+    // socketIo init.
+    const WS_URL = import.meta.env.VITE_WS_URL;
+    if (WS_URL !== undefined) {
+      const _socketIo = io.connect(WS_URL);
+      _socketIo.on("connect", () => {
+        console.log("socket connected");
+      });
+      _socketIo.on("update", (data) => {
+        //console.log(data);
+
+        setStockExecutionPrice(data[1]);
+        // console.log(stockExecutionPrice);
+      });
+
+      setSocketIo(_socketIo);
+    } else {
+      console.log("WS URL not defined");
+    }
+  }, []);
+
+  // when socketIo modified
+  useEffect(() => {
+    if (socketIo !== null) {
+      // socketIo initiated
+      // 2|005930 - 삼성전자 호가, 1|005930 - 삼성전자 체결가
+      const temp = `1|${stockKey}`;
+      // REGISTER_SUB : 등록, RELEASE_SUB : 해제
+      socketIo.emit("REGISTER_SUB", temp);
+    }
+  }, [socketIo]);
 
   const trade = async (transactionType) => {
     try {
@@ -39,11 +75,31 @@ export default function TradeStockPage() {
     } catch (error) {
       console.error(error);
     }
-  }
+  };
 
   const toggleMarketPrice = () => {
     setIsMarketPrice(!isMarketPrice);
   };
+
+  const callPartyInfo = async () => {
+    try {
+      const response = await fetchPartyInfo(partyKey);
+      setPartyInfo(response);
+    } catch (error) {
+      console.error("모임 정보 데이터 호출 중 에러:", error);
+    }
+  };
+
+  useEffect(() => {
+    callPartyInfo();
+  }, []);
+
+  let deposit = 0;
+  console.log(partyInfo);
+
+  if (partyInfo) {
+    deposit = partyInfo.deposit;
+  }
 
   return (
     <>
@@ -80,11 +136,11 @@ export default function TradeStockPage() {
                 />
               </div>
             )}
-            
+
             <div className="trade-current-price">
               {" "}
               <Image src={LightningIcon} className="lightning-icon" />
-              122,286원
+              {parseInt(stockExecutionPrice).toLocaleString()}원
               {/* 현재가 실시간으로 바뀌어야 함 */}
             </div>
           </div>
@@ -123,11 +179,17 @@ export default function TradeStockPage() {
             type="text"
             value={amount}
             onChange={(e) => setAmount(e.target.value)}
-
           />
 
           <div className="trade-possible">
-            {type === "구매" ? <> 구매 가능 -원 {}</> : <>판매 가능 최대 -주</>}
+            {type === "구매" ? (
+              <>
+                {" "}
+                구매 가능 {deposit.toLocaleString()}원 {}
+              </>
+            ) : (
+              <>판매 가능 최대 -주</>
+            )}
           </div>
         </div>
         <div className="trade-volume-btns">
