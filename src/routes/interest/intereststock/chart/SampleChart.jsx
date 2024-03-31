4b53aeec94a5b4039bdf440ca7cda021cef33e08
bkkmw <roboticts49@gmail.com>
2024-03-31 23:18:55 +0900

    remove: unnecessary codes

--- src/routes/interest/intereststock/chart/SampleChart.jsx
+++ src/routes/interest/intereststock/chart/SampleChart.jsx
@@ -7,33 +7,6 @@ export default function SampleChart({ mode, stockKey }) {
   const [data, setData] = useState([]);
   const [chartType, setChartType] = useState("line");
 
-  // 주식 가격 데이터 생성
-  const generateStockData = () => {
-    const data = [];
-    const numberOfDataPoints = 30; // 데이터 포인트 개수
-
-    let basePrice = 100; // 기준 가격
-
-    for (let i = 1; i <= numberOfDataPoints; i++) {
-      // 주가 랜덤하게 생성
-      const open = basePrice + Math.random() * 10 - 5;
-      const high = open + Math.random() * 5;
-      const low = open - Math.random() * 5;
-      const close = low + Math.random() * (high - low);
-      const date = new Date();
-      date.setDate(date.getDate() - (numberOfDataPoints - i)); // 오늘부터 과거로 거슬러 올라감
-
-      data.push({
-        x: date.toISOString(),
-        y: [open, high, low, close],
-      });
-
-      basePrice = close; // 다음 기준 가격은 이전 종가로 설정
-    }
-
-    return data;
-  };
-
   useEffect(() => {
     async function fetchData() {
       const currentDate = new Date();
@@ -70,8 +43,6 @@ export default function SampleChart({ mode, stockKey }) {
 
     return `${year}${month}${day}${hours}${minutes}`;
   }, [])
-
-  // const data = generateStockData(); // 주식 데이터 생성
 
   return (
     <ApexChart
